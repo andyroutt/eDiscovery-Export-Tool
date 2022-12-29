784c044728bce8a59e1df11f2839837658abeca7
Andy Routt <andy.routt@routt.org>
2022-12-28 23:39:39 -0600

    fix export creation and improve prompting

--- eDiscovery-Export-Tool.ps1
+++ eDiscovery-Export-Tool.ps1
@@ -28,7 +28,6 @@ if ($GLOBAL:DebugPreference -eq "Continue"){
 $Format = "FxStream"
 $SharePointArchiveFormat = "SingleZip"
 $ExchangeArchiveFormat = "PerUserPst"
-$Dedupe = $false
 
 # Create Region Table
 $regiontable = New-Object System.Data.Datatable
@@ -119,23 +118,13 @@ Function Get-MailFormat($Format){
 }
 
 # Show Friendly Dedupe Name
-Function Get-DedupStatus($Dedupe){
+Function Get-Dedupe($Dedupe){
     Switch ($Dedupe)
     {
         "True"      {$dedupestatus = "Yes"}
         "False"     {$dedupestatus = "No"}
     }
     return $dedupestatus
-}
-
-# Show Friendly Dedupe Name
-Function Get-Scope($askscope){
-    Switch ($askscope)
-    {
-        "Y"         {$scope = "BothIndexedAndUnindexedItems"}
-        "N"         {$scope = "IndexedItemsOnly"}
-    }
-    return $scope
 }
 
 # Connect to Compliance Center
@@ -159,33 +148,39 @@ Function Create-ExportJob($SearchName, $Region, $Format, $SharePointArchiveForma
 
     # Create New Export
     if ($search = Get-ComplianceSearch -identity $SearchName -erroraction SilentlyContinue){
-        if ($search."Status" -ne "Completed"){
-            write-host "Waiting for Search to Finish ..." -NoNewline -ForegroundColor Green
+        New-ComplianceSearchAction `
+        -SearchName $SearchName `
+        -Region $Region `
+        -Format $Format `
+        -SharePointArchiveFormat $SharePointArchiveFormat `
+        -ExchangeArchiveFormat $ExchangeArchiveFormat `
+        -Scope $Scope `
+        -EnableDedupe $Dedupe `
+        -Export
+        | Out-Null
+
+        # Wait for Export Job to Complete
+        $JobName = $SearchName+"_Export"
+        $job = Get-ComplianceSearchAction -Identity $JobName -includeCredential -erroraction SilentlyContinue
+        if ($job."Status" -ne "Completed"){
+            write-host ""
+            write-host "Creating Export Job ." -NoNewline -ForegroundColor Green
             Start-Sleep -s 1
-            while ($search."Status" -ne "Completed"){
+            while ($job."Status" -ne "Completed"){
                 write-host "." -NoNewline -ForegroundColor Green
                 Start-Sleep -s 5
-                $search = Get-ComplianceSearch -identity $SearchName -erroraction SilentlyContinue
+                $job = Get-ComplianceSearchAction -Identity $JobName -includeCredential -erroraction SilentlyContinue
             }
             write-host " Done!" -ForegroundColor Green
+            write-host ""
         }
-        New-ComplianceSearchAction `
-            -SearchName $SearchName `
-            -Region $Region `
-            -Format $Format `
-            -SharePointArchiveFormat $SharePointArchiveFormat `
-            -ExchangeArchiveFormat $ExchangeArchiveFormat `
-            -Scope $Scope `
-            -EnableDedupe $Dedupe `
-            -Export
-            | Out-Null
+
     } else {
         write-host ""
         write-host "ERROR! Search Not Found!" -ForegroundColor Red
         write-host ""
         break
     }
-    Write-host "Export Job Created" -ForegroundColor Yellow
 }
 
 # Show Export Job
@@ -193,9 +188,9 @@ Function Show-NewExportJob($SearchName, $Region, $Format, $SharePointArchiveForm
 
     $regionname = Get-RegionName $Region
     $itemformat = Get-MailFormat $Format
-    $dedupestatus = Get-DedupStatus $Dedupe
+    $dedupestatus = Get-Dedupe $Dedupe
 
-    write-host ""    
+    write-host ""
     write-host "Search Name: $SearchName" -ForegroundColor Yellow
     write-host "Region Name: $regionname" -ForegroundColor Yellow
     write-host ""
@@ -204,7 +199,7 @@ Function Show-NewExportJob($SearchName, $Region, $Format, $SharePointArchiveForm
     write-host "SharePoint Output: $SharePointArchiveFormat" -ForegroundColor Yellow
     write-host "Scope: $Scope" -ForegroundColor Yellow
     write-host "Dedupe: $dedupestatus" -ForegroundColor Yellow
-    Write-host ""
+    write-host ""
 }
 
 # Show Export Job
@@ -230,8 +225,9 @@ Function Show-FinishedExportJob($SearchName){
             $exportname = $export."Name"
             $size = Format-ByteSize $e_size
 
-            write-host "Case Name: $casename"
-            write-host "Export Job: $exportname"
+            write-host ""
+            write-host "Case Name: $casename" -ForegroundColor Blue
+            write-host "Export Job: $exportname" -ForegroundColor Blue
             write-host ""
             write-host "Total Sources: $s_sources" -ForegroundColor Blue
             write-host "Failed Sources: $f_sources" -ForegroundColor Blue
@@ -241,6 +237,7 @@ Function Show-FinishedExportJob($SearchName){
             write-host ""
             Write-Debug "Blob URL: $bloburl"
             Write-Debug "SAS Key: $sastoken"
+            Write-Debug ""
 
         }
 
@@ -281,7 +278,7 @@ Function Get-ExportData($ExportPath, $SearchName, $UnifiedExportTool){
     $downLoadProcess = Start-Process -FilePath $UnifiedExportTool -ArgumentList $arguments -Windowstyle Normal -RedirectStandardError $e_log -PassThru
     $proc = Get-Process | where -Property name -EQ "microsoft.office.client.discovery.unifiedexporttool"
     write-host ""
-    write-host "Downloading ..." -NoNewline -ForegroundColor Grey
+    write-host "Downloading ." -NoNewline -ForegroundColor Green
     
     # Wait for Download to Finish
     Start-Sleep -s 1
@@ -291,8 +288,8 @@ Function Get-ExportData($ExportPath, $SearchName, $UnifiedExportTool){
             Start-Sleep -s 5
         }
     }
-    write-host " Done!" -ForegroundColor Grey
-    write-host
+    write-host " Done!" -ForegroundColor Green
+    write-host ""
 }
 
 ########
@@ -306,7 +303,6 @@ Connect-ComplianceCenter
 $SearchName = Read-Host -Prompt "Enter Name of Search"
 $ExportPath = Read-Host -Prompt "Export Path"
 $Region = Read-Host -Prompt "Region"
-$askscope = Read-Host -Prompt "Include Unindexed Items (Y/N)"
 
 # Check Validate Region Code
 $regionname = Get-RegionName $region
@@ -317,33 +313,69 @@ while (!($regionname)){
     $regionname = Get-RegionName $region
 }
 
-# Check Scope
-$Scope = Get-Scope $askscope
-while (!($Scope)){
-    write-host "ERROR! Incorrect Response!" -ForegroundColor Red
-    $askscope = Read-Host -Prompt "Try Again"
-    $Scope = Get-Scope $askscope
+write-host ""
+
+# Set Scope
+$title = ""
+$message = "Include Unindexed Items?"
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Include Unindexed Items"
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Do Not Include Unindexed Items"
+$cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel","Cancel."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no, $cancel)
+$result = $host.ui.PromptForChoice($title, $message, $options, 1)
+
+switch ($result)
+{
+    0 { $scope = "BothIndexedAndUnindexedItems" }
+    1 { $scope = "IndexedItemsOnly" }
+    2 { return; break }
+}
+
+# Set Dedupe
+$title = ""
+$message = "Deduplicate Items?"
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Deduplicate Items"
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Do Not Deduplicate Items"
+$cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel","Cancel."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no, $cancel)
+$result = $host.ui.PromptForChoice($title, $message, $options, 1)
+
+switch ($result)
+{
+    0 { $Dedupe = $true }
+    1 { $Dedupe = $false }
+    2 { return; break }
 }
 
 # Create Export Job
 Show-NewExportJob $SearchName $Region $Format $SharePointArchiveFormat $ExchangeArchiveFormat $Scope $Dedupe
 
 # Prompt for Creating Job
-$exportprompt = Read-Host -Prompt "Create Export Job (Y/N): "
-if ($exportprompt -eq "Y"){
-    Create-ExportJob $SearchName $Region $Format $SharePointArchiveFormat $ExchangeArchiveFormat $Scope $Dedupe
-} else {
-    write-host "Abort" -ForegroundColor Red
-    break
+$title = ""
+$message = "Create Export Job?"
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Create Export Job"
+$cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel","Cancel."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $cancel)
+$result = $host.ui.PromptForChoice($title, $message, $options, 0)
+
+switch ($result)
+{
+    0 { Create-ExportJob $SearchName $Region $Format $SharePointArchiveFormat $ExchangeArchiveFormat $Scope $Dedupe }
+    1 { return; break }
 }
 
 # Prompt for Download
-$downloadprompt = Read-Host -Prompt "Download Export Data (Y/N): "
-if ($downloadprompt -eq "Y"){
-    Get-ExportData $ExportPath $SearchName $UnifiedExportTool
-} else {
-    write-host "Abort" -ForegroundColor Red
-    break
+$title = ""
+$message = "Download Data?"
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Download Data"
+$cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel","Cancel."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $cancel)
+$result = $host.ui.PromptForChoice($title, $message, $options, 0)
+
+switch ($result)
+{
+    0 { Get-ExportData $ExportPath $SearchName $UnifiedExportTool }
+    1 { return; break }
 }
 
 # Show Export Job Detail
